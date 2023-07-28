3443ccab6ca47d05f46959e4d8b27cefe8d263f2
Maria <yarosl-maria@yandex.ru>
2023-07-28 14:06:23 +0300

    start-page: добавлены стили

--- src/pages/StartPage/StartPage.jsx
+++ src/pages/StartPage/StartPage.jsx
@@ -1,11 +1,14 @@
 import { useNavigate } from 'react-router-dom';
+import classes from './StartPage.module.css'
 
 const StartPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <button onClick={() => navigate('/table')}>
+    <div className={classes.container}>
+      <button
+        className={classes.btn}
+        onClick={() => navigate('/table')}>
         К таблице
       </button>
     </div>
